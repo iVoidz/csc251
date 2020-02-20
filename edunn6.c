8e79138a14707b7dcbbe5f5d03dcd6b547d756c3
ivoidz <elijahkdunn19@gmail.com>
2020-02-20 10:35:39 -0800

    lab 6 final

--- edunn6.c
+++ edunn6.c
@@ -1,25 +1,43 @@
-/*
-Elijah Dunn
-2/10/2020
-csc-251
-*/
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+/*ELijah Dunn
+ * 2/20/2020
+ * csc-251
+ * lab 6
+ * */
+#include<stdio.h>
+#include<stdlib.h>
 
 int main(void)
 {
-	int x=0,y=0,z=0;
-	srand(time(NULL));
+	int firstArray[] = {0,2,4,6,8};
+	int secondArray[] = {1,3,5,7,9};
 	int i;
-	for(i=0; i < 10; i++)
+	
+	printf("\nBefore Swap");
+
+	//PRINT BEFORE
+	for(i = 0; i < 5; i++)
 	{
-		x = (rand()%(80-50+1))+50; 
-		printf("x = %d \n",x);
+		printf("\nfirstArray[i] = %d, secondArray[i] = %d",firstArray[i],secondArray[i]);
 	}
 
+	//SWAP
+	for(i = 0; i < 5; i++)
+	{
+		int swapped = firstArray[i];
+		firstArray[i] = secondArray[i];
+		secondArray[i] = swapped;
+	}
+
+	//space inbetween
+	printf("\nAfter Swap");
+
+	//PRINT AFTER
+	for(i = 0; i < 5; i++)
+	{
+		printf("\nfirstArray[i] = %d, secondArray[i] = %d", firstArray[i],secondArray[i]);
+	}
+	
+	printf("\n");
+	
 	return EXIT_SUCCESS;
-
-
 }
